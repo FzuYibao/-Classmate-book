726ee774eee3d014dda4b1554b403ad51a101c28
Maple27 <654686099@qq.com>
2017-11-04 10:26:58 +0800

    10:26

---
+++ app/src/main/java/com/fzu2015/classmate_book/adapter/MemberAdapter.java
@@ -0,0 +1,36 @@
+package com.fzu2015.classmate_book.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Maple27 on 2017/11/4.
+ */
+
+public class MemberAdapter extends BaseAdapter{
+
+    public MemberAdapter() {
+        super();
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        return null;
+    }
+}
