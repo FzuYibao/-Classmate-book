43e7718ec3fbcf730e1e851addd87f75d51b66a5
Maple27 <654686099@qq.com>
2017-11-04 08:56:50 +0800

    APP框架

---
+++ app/src/main/java/com/fzu2015/classmate_book/LoginActivity.java
@@ -0,0 +1,10 @@
+package com.fzu2015.classmate_book;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Maple27 on 2017/11/4.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
